7ecda9d40aba49b136fbaf61d3a19c4551aa74fc
yunnjj72 <joyjung040@gmail.com>
2025-05-09 14:50:43 +0900

    node.js 시작

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('서버 정상 작동 중');
+});
+
+app.listen(port, () => {
+  console.log(`서버 실행 중! http://localhost:${port}`);
+});
+
+app.post('/signup', (req, res) => {
+    const { email, password, nickname } = req.body;
+  
+    if (!email || !password || !nickname) {
+      return res.status(400).json({ message: '모든 항목을 입력하세요!' });
+    }
+  
+    console.log('회원가입 요청:', req.body);
+  
+    return res.status(201).json({ message: '회원가입 성공!' });
+  });
+  
