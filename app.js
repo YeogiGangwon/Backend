3c992bb6248a31f99b7a2a135cb0e8fad04bfe7c
yunnjj72 <joyjung040@gmail.com>
2025-05-30 11:01:18 +0900

    수정

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 const express = require('express');
-require('dotenv').config();
 const app = express();
 const userRoutes = require('./routes/user.routes');
 const placeRoutes = require('./routes/place.routes');
 const favoriteRoutes = require('./routes/favorite.routes');
 const weatherRoutes = require('./routes/weather.routes');
-const recommendationRoutes = require('./routes/recommendation.routes');
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/places', placeRoutes);
 app.use('/api/favorites', favoriteRoutes);
 app.use('/api/weather', weatherRoutes);
-app.use('/api/recommendations', recommendationRoutes);
-
-const PORT = process.env.PORT || 8080;
-
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
 
 module.exports = app;
